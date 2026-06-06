3f7498f79fccfa51af1224d8e743961081945290
Korneplod1337 <korneewegor@gmail.com>
2026-06-07 02:56:57 +0300

    sound manager  + card texture fix

--- game/objects/Coins/coin_bag.gd
+++ game/objects/Coins/coin_bag.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
-		GameState.add_coins(randi_range(2,5))
+		var coins := randi_range(2, 5)
+		GameState.add_coins(coins)
+		SoundManager.play_coins(coins)
 		queue_free()
-	
+
 func _ready() -> void:
 	$AnimatedSprite2D.play()
---
+++ z_global_scripts/SoundManager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+const COIN_SFX := preload("uid://djw0tdknh18x6")
+
+func play(stream: AudioStream, volume_db: float = 0.0, pitch_scale: float = 1.0) -> void:
+	var player := AudioStreamPlayer.new()
+	player.stream = stream
+	player.volume_db = volume_db
+	player.pitch_scale = pitch_scale
+	add_child(player)
+	player.finished.connect(player.queue_free, CONNECT_ONE_SHOT)
+	player.play()
+
+
+
+func play_coins(count: int, delay: float = 0.1) -> void:
+	for i in count:
+		if i > 0:
+			await get_tree().create_timer(delay).timeout
+		play(COIN_SFX, 4.0, 0.9)
